005b4f5e688ba0a7e2c1ef018fbeb60fef6be9a7
AnaelleChiles <118526850+AnaelleChiles@users.noreply.github.com>
2023-10-18 15:37:17 +0200

    Base pour créer un template

--- blog/index.php
+++ blog/index.php
@@ -1,46 +1,7 @@
-<!DOCTYPE html>
-<html>
+<?php
 
-<head>
-   <meta charset="utf-8" />
-   <title>Le blog de l'AVBN</title>
-   <link href="style.css" rel="stylesheet" />
-</head>
+require('src/model.php');
 
-<body>
-   <?php
-   // Connexion à la base de données
-   try {
-      $database = new PDO(
-         'mysql:host=localhost;dbname=blog;charset=utf8',
-         'root',
-         'root'
-      );
-   } catch (Exception $e) {
-      die('Erreur : ' . $e->getMessage());
-   }
-   // We retrieve the 5 last blog posts.
-   $statement = $database->query(
-      "SELECT id, titre, contenu, DATE_FORMAT(date_creation,
-         '%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr
-         FROM billets ORDER BY date_creation DESC LIMIT 0, 5"
-   );
-   $posts = [];
-   while (($row = $statement->fetch())) {
-      $post = [
-         'title' => $row['titre'],
-         'french_creation_date' => $row['date_creation_fr'],
-         'content' => $row['contenu'],
-      ];
-      $posts[] = $post;
-   }
+$posts = getPosts();
 
-
-   require('src/model.php');
-   $posts = getPosts();
-   require('templates/homepage.php');
-
-   ?>
-</body>
-
-</html>
+require('templates/homepage.php');
