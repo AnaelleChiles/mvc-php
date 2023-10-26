eb9bb53f099cc873245db6466bd1ac31b2f2a121
AnaelleChiles <118526850+AnaelleChiles@users.noreply.github.com>
2023-10-26 14:06:17 +0200

    Mettez à jour le modèle

--- blog/src/model.php
+++ blog/src/model.php
@@ -1,12 +1,14 @@
 <?php
 
-function getPosts() {
-    try {
+function dbConnect(){
+	 try {
         $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
     } catch(Exception $e) {
         die('Erreur : '.$e->getMessage());
     }
+}
 
+function getPosts() {
     $statement = $database->query(
         "SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM posts ORDER BY creation_date DESC LIMIT 0, 5"
     );
@@ -23,4 +25,41 @@ function getPosts() {
     }
 
     return $posts;
+}
+function getPost($identifier) {
+	dbConnect();
+	$statement = $database->prepare(
+	"SELECT id, title, content,
+	DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss')
+	AS french_creation_date FROM posts WHERE id = ?"
+	);
+	$statement->execute([$identifier]);
+	$row = $statement->fetch();
+	$post = [
+		'title' => $row['title'],
+		'french_creation_date' => $row['french_creation_date'],
+		'content' => $row['content'],
+	];
+	return $post;
+}
+function getComments($identifier)
+{
+	dbConnect();
+	$statement = $database->prepare(
+	"SELECT id, author, comment,
+	DATE_FORMAT(comment_date, '%d/%m/%Y à %Hh%imin%ss')
+	AS french_creation_date FROM comments WHERE post_id = ?
+	ORDER BY comment_date DESC"
+	);
+	$statement->execute([$identifier]);
+	$comments = [];
+	while (($row = $statement->fetch())) {
+		$comment = [
+		'author' => $row['author'],
+		'french_creation_date' => $row['french_creation_date'],
+		'comment' => $row['comment'],
+		];
+		$comments[] = $comment;
+	}
+	return $comments;
 }
