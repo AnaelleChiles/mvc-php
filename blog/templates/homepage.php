200e474fa18beda607cddb317d4df9a7cb4474d9
AnaelleChiles <118526850+AnaelleChiles@users.noreply.github.com>
2023-11-06 15:47:40 +0100

    Base pour passer à une architecture MVC OO

--- blog/templates/homepage.php
+++ blog/templates/homepage.php
@@ -1,33 +1,26 @@
-<!DOCTYPE html>
-<html>
+<?php $title = "Le blog de l'AVBN"; ?>
 
-<body>
-    <?php $title = "Le blog de l'AVBN"; ?>
-    <?php ob_start(); ?>
-    <h1>Le super blog de l'AVBN !</h1>
-    <p>Derniers billets du blog :</p>
+<?php ob_start(); ?>
+<h1>Le super blog de l'AVBN !</h1>
+<p>Derniers billets du blog :</p>
 
-    <?php
-    foreach ($posts as $post) {
-        ?>
-        <div class="news">
-            <h3>
-                <?= htmlspecialchars($post['title']); ?>
-                <em>le
-                    <?= $post['french_creation_date']; ?>
-                </em>
-            </h3>
-            <p>
-                <?= nl2br(htmlspecialchars($post['content'])); ?>
-                <br />
-                <em><a href="index.php?action=post&id=<?= urlencode($post['identifier']) ?>">Commentaires</a></em>
-            </p>
-        </div>
-        <?php
-    }
-    ?>
-    <?php $content = ob_get_clean(); ?>
-    <?php require('layout.php') ?>
-</body>
+<?php
+foreach ($posts as $post) {
+?>
+    <div class="news">
+        <h3>
+            <?= htmlspecialchars($post['title']); ?>
+            <em>le <?= $post['french_creation_date']; ?></em>
+        </h3>
+        <p>
+            <?= nl2br(htmlspecialchars($post['content'])); ?>
+            <br />
+            <em><a href="index.php?action=post&id=<?= urlencode($post['identifier']) ?>">Commentaires</a></em>
+        </p>
+    </div>
+<?php
+}
+?>
+<?php $content = ob_get_clean(); ?>
 
-</html>
+<?php require('layout.php') ?>
