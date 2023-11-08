bc467418f100da665c4db9845e2bdd7ebbab730e
AnaelleChiles <chiles.anaelle@gmail.com>
2023-11-08 10:48:57 +0100

    Tirez parti de la composition

--- blog/src/controllers/homepage.php
+++ blog/src/controllers/homepage.php
@@ -1,11 +1,10 @@
 <?php
 
 require_once('src/model/post.php');
-
 function homepage()
 {
     $postRepository = new PostRepository();
+    $postRepository->connection = new DatabaseConnection();
     $posts = $postRepository->getPosts();
-
     require('templates/homepage.php');
 }
